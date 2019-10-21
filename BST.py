e5f9d014513c6f399fd9309b57f44fa44205d5aa
krishkilaparthi323 <56755627+krishkilaparthi323@users.noreply.github.com>
2019-10-21 14:17:38 +0530

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,30 @@
+class Node():
+    def __init__(self,value):
+        self.value=value
+        self.left=None
+        self.right=None
+
+class BinaryTree(object):
+    def __init__(self,root):
+        self.root= Node(root)
+
+    def height(self,Node):
+        if Node is None:
+            return -1
+        left_height=self.height(Node.left)
+        right_height=self.height(Node.right)
+        return 1+ max(left_height,right_height)
+
+tree=BinaryTree(1)
+tree.root.left=Node(2)
+tree.root.right=Node(3)
+tree.root.left.left=Node(4)
+tree.root.left.right=Node(5)
+
+print(tree.height(tree.root))
+
+
+
+
+
+
